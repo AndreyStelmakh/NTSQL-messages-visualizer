6ce034e3f277700383c912a356d18787e19a06f9
AndreyStelmakh <andrey.stelmakh@live.ru>
2017-01-28 13:36:32 +0300

    first commit

---
+++ NtsqlMessagesVisualizerControl/NtsqlMessagesVisualizerControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Xml.Linq;
+
+namespace Lmx.UI.Controls
+{
+    [TemplatePart(Name = NtsqlMessagesVisualizerControl.ElementViewRichEditBox, Type = typeof(RichTextBox))]
+    public class NtsqlMessagesVisualizerControl : Control
+    {
+        static NtsqlMessagesVisualizerControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NtsqlMessagesVisualizerControl), new FrameworkPropertyMetadata(typeof(NtsqlMessagesVisualizerControl)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _viewRichEditBox = GetTemplateChild(ElementViewRichEditBox) as RichTextBox;
+        }
+
+        private static FlowDocument ToFlowDocument(XDocument xDocument)
+        {
+            throw new NotImplementedException();
+            return new FlowDocument();
+        }
+
+        public XDocument Value
+        {
+            get { return (XDocument)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(XDocument), typeof(NtsqlMessagesVisualizerControl), new PropertyMetadata(new PropertyChangedCallback((d, e) =>
+            {
+                var ctrl = d as NtsqlMessagesVisualizerControl;
+
+                if(ctrl!= null && e.NewValue is XDocument)
+                {
+                    ctrl._viewRichEditBox.Document = ToFlowDocument((XDocument)e.NewValue);
+                }
+            })));
+
+        private RichTextBox _viewRichEditBox;
+
+        private const string ElementViewRichEditBox = "PART_View";
+    }
+}
---
+++ WPF startup project/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WPF_startup_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public XDocument Ntsqlmessage
+        {
+            get
+            {
+                return _ntsqlmessage;
+            }
+
+            set
+            {
+                _ntsqlmessage = value;
+
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Ntsqlmessage"));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private XDocument _ntsqlmessage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Ntsqlmessage = XDocument.Parse("<err text='Сохранение документа'><err text='Повторяющиеся получатели'><err text='34563'/><err text='234'/><err text='65564'/></err></err>");
+        }
+    }
+}
